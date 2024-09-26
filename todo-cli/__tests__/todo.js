cd9255ce7956881d669e8d5eb7969517dbc6430c
Gnani Sai <gnaneswarsai117@gmail.com>
2024-09-26 21:16:15 +0530

    Add other function test cases

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,22 +1,34 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("Todolist Test Suite", () => {
+  const today = new Date().toISOString().slice(0, 10);
+  const yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
+    .toISOString()
+    .slice(0, 10);
+  const tommorrow = new Date(new Date().setDate(new Date().getDate() + 1))
+    .toISOString()
+    .slice(0, 10);
   beforeAll(() => {
     add({
-      title: "Test todo",
+      title: "Test todo1",
       completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
+      dueDate: tommorrow,
+    });
+    add({
+      title: "Test todo2",
+      completed: false,
+      dueDate: yesterday,
     });
   });
   test("Should add new todo", () => {
     const todoItemsCount = all.length;
     add({
-      title: "Test todo",
+      title: "Test todo3",
       completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
+      dueDate: today,
     });
     expect(all.length).toBe(todoItemsCount + 1);
   });
@@ -25,5 +37,23 @@ describe("Todolist Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("Should retrieve overdue items", () => {
+    const data = overdue();
+    expect(data.length).toBe(1);
+    expect(data[0].title).toBe("Test todo2");
+  });
+
+  test("Should retrieve due today items", () => {
+    const data = dueToday();
+    expect(data.length).toBe(1);
+    expect(data[0].title).toBe("Test todo3");
+  });
+
+  test("Should retrieve due later items", () => {
+    const data = dueLater();
+    expect(data.length).toBe(1);
+    expect(data[0].title).toBe("Test todo1");
   });
 });
