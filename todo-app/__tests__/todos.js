6586560fa75a209381d62e6334e1da2d9bfe2bf2
Gnani Sai <gnaneswarsai117@gmail.com>
2024-10-02 12:00:36 +0530

    Update deleteTodo tests

--- todo-app/__tests__/todos.js
+++ todo-app/__tests__/todos.js
@@ -73,10 +73,8 @@ describe("Todo Application", function () {
 
   test("Deletes a todo with the given ID if it exists and sends a boolean response", async () => {
     const response = await agent.get("/todos").send();
-    const parsedResponse = JSON.parse(response.text);
-    const todoID = parsedResponse.id;
     expect(response.statusCode).toBe(200);
-    const deletedTodo = await agent.put(`/todos/${todoID}`).send();
+    const deletedTodo = await agent.put(`/todos/1`).send();
     expect(deletedTodo.statusCode).toBe(404);
   });
 });
